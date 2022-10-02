9e8c3f10ce569f625314b4ed94bfda1a964316f1
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-10-03 00:03:02 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\profile;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +22,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        profile::create(array(
+            "id" => 1,
+            "created_at" => "2022-10-02 14:43:12",
+            "updated_at" => "2022-10-02 14:51:06",
+            "logo" => "upload/logo-putih-removebg-preview.png",
+            "nama" => "TemanCoret.Std",
+            "deskripsi" => "Brand Lokal Makassar",
+            "ig" => "https://www.instagram.com/temancoret.studio/",
+            "fb" => NULL,
+            "wa" => NULL,
+            "alamat" => NULL,
+        ));
     }
 }
